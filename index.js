1c0f9711a068669105537b06528790589433f52d
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-07-07 00:04:04 +0200

    Print Printable Characters

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // import
-const fs = require('fs')
 const chalk = require('chalk')
+const fs = require('fs')
 const hsl2rgb = require('pure-color/convert/hsl2rgb')
 
 // config
-const WIDTH = Math.floor(process.stdout.columns / 3)
+const WIDTH = Math.floor(process.stdout.columns / 2)
 const FILE = process.argv[2]
 const CHUNK_SIZE = 1024
 
@@ -13,9 +13,10 @@ const colorHexTable = (() => {
 	const result = []
 	for (let i = 0; i < 256; i++) {
 		const rgb = hsl2rgb([i / 256 * 360, 100, 50]).map(i => Math.floor(i))
-		const string = ((i >> 4).toString(16) +
-			(i & 0xf).toString(16)).toUpperCase()
-		result.push(chalk.black.bgRgb(...rgb)(string) + chalk.white.bgBlack(' '))
+		let string
+		if (i >= 32 && i <= 126) string = String.fromCharCode(i) + ' '
+		else string = ((i >> 4).toString(16) + (i & 0xf).toString(16)).toUpperCase()
+		result.push(chalk.black.bgRgb(...rgb)(string))
 	}
 	return result
 })()
