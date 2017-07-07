b7693fbcedd6be2c47bedd8dcb2a044fd6b4ed3f
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-07-07 03:52:58 +0200

    Implement Address

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const hsl2rgb = require('pure-color/convert/hsl2rgb')
 
 // config
-const WIDTH = Math.floor(process.stdout.columns / 2)
+const WIDTH = Math.floor(process.stdout.columns / 2) - 5
 const FILE = process.argv[2]
 const CHUNK_SIZE = 1024
 
@@ -22,12 +22,34 @@ const colorHexTable = (() => {
 })()
 
 // helper
+const address = offset =>
+	[
+		(offset & 0xf0000000) >> 28,
+		(offset & 0x0f000000) >> 24,
+		(offset & 0x00f00000) >> 20,
+		(offset & 0x000f0000) >> 16,
+		(offset & 0x0000f000) >> 12,
+		(offset & 0x00000f00) >> 8,
+		(offset & 0x000000f0) >> 4,
+		offset & 0x0000000f
+	]
+		.map(i => i.toString(16))
+		.join('')
+		.toUpperCase()
+
+// helper
 const print = (buffer, offset) => {
 	const toPrint = []
-	for (let i = 0; i < buffer.length; i++)
-		toPrint[i] =
-			colorHexTable[buffer[i]] +
-			((i + offset) % WIDTH === WIDTH - 1 ? '\n' : '')
+	for (let i = 0; i < buffer.length; i++) {
+		const position = offset + i
+		const mod = position % WIDTH
+
+		if (mod === 0) toPrint.push(address(position) + ' ')
+
+		toPrint.push(colorHexTable[buffer[i]])
+
+		if (mod === WIDTH - 1) toPrint.push('\n')
+	}
 	process.stdout.write(toPrint.join(''))
 }
 
@@ -60,5 +82,6 @@ fs.stat(FILE, (error, stat) => {
 				print(buffer, i * CHUNK_SIZE)
 			}
 		}
+		console.log()
 	})
 })
